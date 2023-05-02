55ed380ec70d319c1bbff22dbbb49c324d2c3842
Mikita Shykin <mikita.shikin@gmail.com>
2023-05-02 16:32:28 +0200

    add account class

---
+++ src/Account.java
@@ -0,0 +1,17 @@
+import static java.lang.System.out;
+public class Account {
+    String name;
+    String address;
+    double balance;
+    public void display() {
+        out.print(name);
+        out.print(" (");
+        out.print(address);
+        out.print(") imeet na schetu $");
+        out.print(balance);
+    }
+    public double getInterest (double percentageRate){
+        return balance * percentageRate/100.00;
+
+    }
+}
